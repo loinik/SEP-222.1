ac1806af75f81de98ffc63992ba1ff43ecf8db47
Mikel <>
2024-01-22 20:11:36 +0600

    Header text added

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let header = document.createElement("h1");
+header.textContent = "Hello, World!";
 header.style.fontFamily = "sans-serif";
 header.style.color = "darkblue";
 document.body.append(header);
